8aa5e28cdc89365b537426412ebed39fae8a4699
abemaru <awebo.abemaru@gmail.com>
2025-05-22 18:44:05 +0900

    WIP:todo追加用のモーダル作成中

---
+++ src/components/modals/AddTodoModal.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+defineProps({
+  visible: Boolean
+})
+const emit = defineEmits(['close', 'submit'])
+const todoTitle = ref('')
+
+watch(() => visible, (newVal) => {
+  if (newVal) {
+    todoTitle.value = ''
+  }
+  closeModal()
+})
+
+function submit(): void {
+  if (!todoTitle.value.trim()) return
+  emit('submit', {
+    todoTitle: todoTitle.value
+  })
+  closeModal()
+}
+
+function closeModal(): void {
+  emit('close')
+}
+</script>
+
+<template>
+  <div v-if="visible" class="modal-overlay" @click.self="closeModal">
+    <div class="modal-contents">
+      <header>
+        <slot name="header">Add Your Todo</slot>
+      </header>
+      <section class="modal-body">
+        <form @submit.prevent="submit">
+          <div class="form-group">
+            <label for="title">Task Name</label>
+            <input id="title" v-model="todoTitle" type="text" required placeholder="study vue3..." />
+          </div>
+        </form>
+      </section>
+      <footer class="modal-footer">
+        <slot name="footer">
+          <button @click="closeModal">close</button>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
